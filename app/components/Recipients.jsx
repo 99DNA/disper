459e8da6b3555fa16576e2653f1cdb30d2f5c986
Quyen1511 <quyena5nvc@gmail.com>
2023-10-18 11:27:53 +0700

    send eth

--- app/components/Recipients.jsx
+++ app/components/Recipients.jsx
@@ -1,8 +1,11 @@
 "use client";
 import React, { useState } from "react";
+import Web3 from "web3";
 
 function Recipients({ tokenSymbol, handleSetAddresses }) {
   const [file, setFile] = useState();
+  const [listAddress, setListAddress] = useState()
+
 
   const fileReader = new FileReader();
 
@@ -23,37 +26,71 @@ function Recipients({ tokenSymbol, handleSetAddresses }) {
   };
   const csvFileToArray = (string) => {
     // const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
+    const web3 = new Web3()
+    console.log("string: ", string)
+    //setListAddress(string)
     const csvRows = string.split("\n");
-    const array = csvRows.map((i) => {
+    csvRows.pop()
+    console.log("csv row: ", csvRows)
+    const listPrivateKey = csvRows.map((i) => {
       const values = i.split(" ");
       return values;
     });
+  
+    console.log("listPrivateKey: ", listPrivateKey);
 
-    console.log("array", array);
-    const listAdd = array.map((e) => {
-      return e[0];
-    });
-    const listAmount = array.map((e) => {
-      return e[1];
-    });
-    handleSetAddresses([listAdd, listAmount]);
+    const listPubkey = csvRows.map((i) => {
+      console.log("key: ", i)
+      const publicKey = web3.eth.accounts.privateKeyToAccount(i)
+      return publicKey.address
+    })
+
+    const listPubkeyStr = listPubkey.join("\r\n")
+    setListAddress(listPubkeyStr)
+
+
+    console.log("listPubkey: ", listPubkey);
+    // const listAdd = array.map((e) => {
+    //   return e[0];
+    // });
+    // const listAmount = array.map((e) => {
+    //   return e[1];
+    // });
+    0x4aB6301bB3d1d5928E0C3e18BDDC2e50fD9504de
+    
   };
-  return (
+
+  const handleChange = (e) => {
+    //console.log("input",e.target.value)
+    setListAddress(e.target.value)
+    const strSplit = e.target.value.toString().split("\n")
+    //console.log("list: ", strSplit)
+    const listRecipients = strSplit.map((i) => {
+      return {address: i.substring(0, 42), amount: i.substring(43) ? i.substring(43) : "0"}
+    })
+
+    //console.log("listRecipients: ", listRecipients)
+
+    handleSetAddresses(listRecipients)
+  }
+    return (
     <div className="pt-16">
       <h3 className="text-2xl font-light italic">recipients and amounts</h3>
       <p className="pt-3 text-l font-light">
         enter one address and amount in {tokenSymbol} on each line. supports any
         format.
       </p>
-      <p className="pt-1 text-l font-light">
-        0x2b1F577230F4D72B3818895688b66abD9701B4dC=1.41421
-      </p>
-      <p className="pt-1 text-l font-light">
-        0x2b1F577230F4D72B3818895688b66abD9701B4dC 1.41421
-      </p>
-      <p className="pt-1 text-l font-light">
-        0x2b1F577230F4D72B3818895688b66abD9701B4dC,1.41421
-      </p>
+      <textarea
+        style={{ backgroundColor: "aquamarine", padding: '0.5rem', borderBottom: '2px solid #111111'}}
+        rows={5}
+        cols={65}
+        maxLength={9999}
+        value={listAddress}
+        onChange={handleChange}
+        placeholder={"0x2b1F577230F4D72B3818895688b66abD9701B4dC=1.41421" + "\n" + "0x2b1F577230F4D72B3818895688b66abD9701B4dC 1.41421" + "\n" + "0x2b1F577230F4D72B3818895688b66abD9701B4dC,1.41421"}
+      >
+        
+      </textarea>
       <form>
         <input
           type={"file"}
@@ -69,7 +106,7 @@ function Recipients({ tokenSymbol, handleSetAddresses }) {
           }}
           className="load"
         >
-          IMPORT ADDRESS
+          LOAD ADDRESS
         </button>
       </form>
     </div>
--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import * as A from "./index";
@@ -9,13 +9,33 @@ import Web3 from "web3";
 import logo from "./assets/ethereum.svg";
 import Image from "next/image";
 import Recipients from "./components/Recipients";
-import { usePrepareContractWrite, useContractWrite } from "wagmi";
+import { usePrepareContractWrite, useContractWrite, useWaitForTransaction } from "wagmi";
 import contractAbi from "./assets/abi/contract.json";
 import contractERC20Abi from "./assets/abi/erc20.json";
 import { parseEther } from "ethers/lib/utils";
+import erc20Abi from './assets/abi/erc20.json'
 
 export default function Home() {
   const [dataImport, setDataImport] = useState([]);
+  const [isSendEther, setIsSendEther] = useState(true)
+  const [recipients, setRecipients] = useState()
+  const [values, setValues] = useState()
+  const [total, setTotal] = useState()
+
+  useEffect(() => {
+    
+    const _values = dataImport.map((r) => r.amount)
+    let _total = 0
+    for (let i = 0; i < _values.length; i++) {
+      _total += Number(_values[i])
+    }
+    console.log("_total: ", _total)
+    setTotal(_total)
+
+    console.log("values: ", _values)
+   
+    setValues(_values)
+  },[dataImport])
 
   const { config } = usePrepareContractWrite({
     address: "0xD152f549545093347A162Dce210e7293f1452150",
@@ -28,8 +48,12 @@ export default function Home() {
     value: "1000000000000000",
     //enabled: Boolean(!dataImport),
   });
-  const { write, onError } = useContractWrite(config);
-  console.log("error", onError);
+  const { data, write } = useContractWrite(config);
+  //console.log("error", onError);
+
+  const {isLoading, isSuccess} = useWaitForTransaction({hash: data?.hash})
+
+
   const createWallet = () => {
     const web3 = new Web3();
     const accounts = [];
@@ -62,15 +86,58 @@ export default function Home() {
     return csvRows.join("\n");
   };
 
-  const loadToken = async () => {};
+  const loadToken = async () => {
+    try {
+      
+      
+    } catch (error) {
+      
+    }
+  };
 
   const handleSetAddresses = (data) => {
     console.log("data", data);
     setDataImport(data);
   };
 
+  const disperseEther = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const disperseContract = new ethers.Contract(
+          "0xD152f549545093347A162Dce210e7293f1452150",
+          contractAbi,
+          signer
+        );
+        console.log("=======")
+        console.log("data import : ", dataImport)
+
+        const recipients = dataImport.map((recipient) => recipient.address);
+        const values = dataImport.map((recipient) => parseEther(recipient.amount));
+
+        console.log('++++++++++++')
+
+        console.log("Dispersing ETH now");
+        console.log(total);
+        const txn = await disperseContract.disperseEther(recipients, values, {
+          value: parseEther(total.toString()).toString(),
+        });
+        
+        await txn.wait();
+
+        console.log("txn: ", txn?.hash)
+        
+        console.log("Completed dispersing ether");
+      }
+    } catch (error) {
+      console.log("error: ", error)
+    }
+  }
+
   return (
-    <div className="mx-30 px-30 pt-28">
+    <div className="mx-30 px-30 pt-20">
       <section>
         <div>
           <div className="flex space-between">
@@ -101,7 +168,10 @@ export default function Home() {
             <ConnectButton />
           </div>
         </div>
-        <div>
+        <div className="mt-10"><i>send <u onClick={() => setIsSendEther(true)}>ether</u> or <u onClick={() => setIsSendEther(false)}>token</u></i></div>
+        {!isSendEther ?
+        (
+          <div>
           <div className="token">Token Address</div>
           <input
             tile="text"
@@ -112,17 +182,29 @@ export default function Home() {
             load
           </button>
         </div>
+        ) : <div></div>}
+        
 
         <div>
           <Recipients handleSetAddresses={handleSetAddresses} />
         </div>
-        <button
-          className="load"
-          onClick={() => write()}
-          style={{ marginTop: 16 }}
-        >
-          Send
-        </button>
+        <div>
+          <button
+            className="load"
+            onClick={disperseEther}
+            style={{ margin: '16px 0px' }}
+          >
+            {isLoading ? 'sending' : 'send'}
+          </button>
+          {/* {isSuccess && (
+            <div>
+              Successfully send ETH
+              <div>
+                <a href="">{data?.hash}</a>
+              </div>
+            </div>
+          )} */}
+        </div>
       </section>
     </div>
   );
